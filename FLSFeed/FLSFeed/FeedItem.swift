db76997828f7e3fe22081d1107c7abbd60e4abbc
Francisco <lopezseoane.f@gmail.com>
2021-05-29 10:01:12 +0100

    Create project

---
+++ FLSFeed/FLSFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FLSFeed
+//
+//  Created by Francisco Lopez on 29/05/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
