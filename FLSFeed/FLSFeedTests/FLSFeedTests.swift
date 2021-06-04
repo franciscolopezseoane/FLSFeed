4c027bde1ec89d5b8abf169de73c630958338752
Francisco Lopez Seoane <lopezseoane.f@gmail.com>
2021-06-04 11:00:22 +0100

    RemoteFeedLoader does not request data upon creation

--- FLSFeed/FLSFeedTests/FLSFeedTests.swift
+++ FLSFeed/FLSFeedTests/FLSFeedTests.swift
@@ -8,26 +8,25 @@
 import XCTest
 @testable import FLSFeed
 
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+
 class FLSFeedTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    
+//FIRST TEST!
+    func test_init_doesNotRequestDataFromURL(){
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
